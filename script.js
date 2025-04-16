38fbd13aef310f85cfbf49583d1fd3f1247600da
Philogelos <51879464+Philogelos@users.noreply.github.com>
2025-04-16 17:17:03 +0300

    mobile nav menu

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 (function smoothScroll(){
-    function scrollSmoothlyTo(element) {
-      element.scrollIntoView({
-        behavior: 'smooth'
-      })
-    }
-    
-    function getIdFromInternalLink(link) {
-      try {
-        return decodeURIComponent(link.getAttribute('href').substring(1))
-      } catch (e) {
-        return link.getAttribute('href').substring(1)
-      }
-    }
-    
-    function handleInternalLinkClick(event) {
-      const link = event.currentTarget
-      const id = getIdFromInternalLink(link)
-      const element = document.getElementById(id)
-      
-      if (!element) return
-      scrollSmoothlyTo(element)
-      history.pushState({ scrollTo: id }, '', link.href)
-      event.preventDefault()
-    }
-    
-    document.querySelectorAll("a[href^='#']").forEach(link => {
-      link.addEventListener('click', handleInternalLinkClick)
+  function scrollSmoothlyTo(element) {
+    element.scrollIntoView({
+      behavior: 'smooth'
     })
+  }
+  
+  function getIdFromInternalLink(link) {
+    try {
+      return decodeURIComponent(link.getAttribute('href').substring(1))
+    } catch (e) {
+      return link.getAttribute('href').substring(1)
+    }
+  }
+  
+  function handleInternalLinkClick(event) {
+    const link = event.currentTarget
+    const id = getIdFromInternalLink(link)
+    const element = document.getElementById(id)
     
-    // ToDo: add smooth scrolling on popstate (when navigating back and forward)
-    // from: https://github.com/DaniBr/wikipedia-smooth-scroll/
+    if (!element) return
+    scrollSmoothlyTo(element)
+    history.pushState({ scrollTo: id }, '', link.href)
+    event.preventDefault()
+  }
+  
+  document.querySelectorAll("a[href^='#']").forEach(link => {
+    link.addEventListener('click', handleInternalLinkClick)
+  })
+  
+  // ToDo: add smooth scrolling on popstate (when navigating back and forward)
+  // from: https://github.com/DaniBr/wikipedia-smooth-scroll/
 })()
+
+// mobile nav var
+const navBar = document.getElementById("main-nav")
+function openNav() { navBar.classList.add("open") }
+function closeNav() { navBar.classList.remove("open") }
+//navBar.querySelector("ul").style.display="none"
+//function openNav() { navBar.querySelector("ul").style.display="" }
+//function closeNav() { navBar.querySelector("ul").style.display="none" }
